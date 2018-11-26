2628b908c37303ad31bbbb068fe05d64c46ef73a
luis <luisalfredorigo@iespaucasesnoves.cat>
2018-11-26 20:59:57 +0100

    busqueda

--- autors.php
+++ autors.php
@@ -3,43 +3,17 @@
  <head>
  <meta charset='UTF-8'>
  <title>AUTORS</title>
- <style>
-    * {
-        text-align:center;
-    }
-    img{
-        margin: auto;
-    }
- </style>
+ <link rel="stylesheet" type="text/css" href="autors.css" media="screen" />
  </head>
  <body>
  <header>
  <img class='imagen' src='IesPau.png'><br/>
  </header>
  <?php
-
-$mysqli = new mysqli("localhost", "root", "", "biblioteca");
-// Comprobar conexion
-if ($mysqli->connect_error) {
-    die("Conexion fallida: " . $mysqli->connect_error);
-}
-echo "Conexion realizada";
-echo "<br>";
-$mysqli->set_charset("utf8");
-
-$orden = "NOM_AUT ASC";
-
-
-
-echo "<br/>";
-echo "<form name='input' action='autors.php' method='post' style='text-align:center;  margin-left:auto;  margin-right:auto;'>";
-echo "<input type='submit' name='nomASC' value='A--Z'>";
-echo "<input type='submit' name='nomDESC' value='Z--A'>";
-echo " ";
-echo "<input type='submit' name='codiASC' value='0--9'>";
-echo "<input type='submit' name='codiDESC' value='9--0'>";
-echo "</form>";
-
+   $orden = "NOM_AUT ASC";
+   if(isset($_POST['oculto'])){
+       $orden = $_POST['oculto'];
+   }
 if(isset($_POST['nomASC'])){
     $orden = "NOM_AUT ASC";
 }
@@ -52,15 +26,52 @@ if(isset($_POST['codiASC'])){
 if(isset($_POST['codiDESC'])){
     $orden = "ID_AUT DESC";
 }
-$query = "SELECT ID_AUT, NOM_AUT FROM AUTORs ORDER BY $orden LIMIT 0,20";
+$mysqli = new mysqli("localhost", "root", "", "biblioteca");
+// Comprobar conexion
+if ($mysqli->connect_error) {
+    die("Conexion fallida: " . $mysqli->connect_error);
+}
+
+//Cerca
+$cerca = "";
+$nom = (isset($_POST['nom'])? $_POST['nom'] : '');
+if($nom != ''){
+ $cerca= "where ID_AUT = '$nom' OR NOM_AUT like '%$nom%' ";
+}
+
+echo "Conexion realizada";
+echo "<br>";
+$mysqli->set_charset("utf8");
+
+echo "<br/>";
+echo "<form name='input' action='autors.php' method='post' style='text-align:center;  margin-left:auto;  margin-right:auto;'>";
+echo "<h1>Introdueix un nom o codi per cercar</h1>";
+echo "Nom o Codi: <input type='text' name='nom' value=''/>";
+echo "<input type='submit' name='buscador' value='CERCAR'/>";
+echo "<br/>";
+echo "<br/>";
+echo "<input type='submit' name='nomASC' value='A--Z'/>";
+echo "<input type='submit' name='nomDESC' value='Z--A'/>";
+echo " ";
+echo "<input type='submit' name='codiASC' value='0--9'/>";
+echo "<input type='submit' name='codiDESC' value='9--0'/>";
+echo "<input type='hidden' name='oculto' value='$orden'/>";
+echo "</form>";
+
+//Query per ordenacio y cerca
+$query = "SELECT ID_AUT, NOM_AUT FROM AUTORs $cerca";
+$query .= "ORDER BY $orden LIMIT 0,20";
+
+//taula
 echo "<table border='1' style='text-align:center;  margin-left:auto;  margin-right:auto;'>";
-if ($cursor = $mysqli->query($query)) {
+if ($cursor = $mysqli->query($query) or die($query)) {
     while ($row = $cursor->fetch_assoc()) {
         echo "<tr><td>" . $row["ID_AUT"] . "</td><td>" . $row["NOM_AUT"] . "</td></tr>";
     }
     $cursor->free();
 }
 $mysqli->close();
+
 ?>
  </body>
  </html>
